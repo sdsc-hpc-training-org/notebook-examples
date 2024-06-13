76718d4d137c4a5e648ed5e99468bb972d5d4cb6
Mary Thomas <marypthomas88@gmail.com>
2024-06-12 19:24:43 -0700

    cleanup old notebooks

--- hpc-notebooks/cpu/python_hpc/activate_python-hpc_env.sh
+++
@@ -1,2 +0,0 @@
-. /share/apps/compute/si2020/miniconda3/etc/profile.d/conda.sh 
-conda activate python-hpc
--- hpc-notebooks/cpu/python_hpc/dask_slurm/launch_scheduler.sh
+++
@@ -1,2 +0,0 @@
-echo "Launching dask scheduler"
-/share/apps/compute/si2020/miniconda3/envs/python-hpc/bin/dask-scheduler --scheduler-file ~/.dask_scheduler.json --host $(hostname)
